9126ce12a44a54bb7c2c498919ccfafd2bc4215b
Jen <124745888+jenstem@users.noreply.github.com>
2024-05-22 10:31:03 -0400

    Add image-upload.js + user-routes.js

--- server/routes/image-upload.js
+++ server/routes/image-upload.js
@@ -1,1 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const multer = require('multer');
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3({
+    apiVersion: '2006-03-01',
+});
 
+const storage = multer.memoryStorage({
+    destination: function (req, file, callback) {
+        callback(null, '');
+    },
+});
+
+const upload = multer({ storage }).single('image');
